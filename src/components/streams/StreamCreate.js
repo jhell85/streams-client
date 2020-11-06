32e323d9b34fcbd85d5180b021c4a767d622013c
jhell85 <jhell85@gmail.com>
2020-11-06 07:57:51 -0800

    wired redux-form to app added some boiler plate to stream create component

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React from "react";
+import { Field, reduxForm } from "redux-form";
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>
-};
+class StreamCreate extends React.Component {
+  // all valid examples of using renderInput()
 
-export default StreamCreate
+  // renderInput(formProps) {
+  //   return (
+  //     <input
+  //       onChange={formProps.input.onChange}
+  //       value={formProps.input.value}
+  //     />
+  //   );
+  // }
+
+  // renderInput(ahh) {
+  //   console.log(ahh);
+  //   return <input {...ahh.input} />;
+  // }
+
+  renderInput({ input }) {
+    return <input {...input} />;
+  }
+
+  render() {
+    return (
+      <form>
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
+      </form>
+    );
+  }
+}
+export default reduxForm({
+  form: "streamCreate",
+})(StreamCreate);
--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -6,7 +6,6 @@ const INITIAL_STATE = {
 };
 
 const authReducer = (state = INITIAL_STATE, action) => {
-  console.log(action);
   switch (action.type) {
     case SIGN_IN:
       return { ...state, isSignedIn: true, userId: action.payload };
