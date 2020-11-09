50f0ca342db60ab118758e0816bcbdf618a03aab
jhell85 <jhell85@gmail.com>
2020-11-09 09:42:52 -0800

    add history file and changed to Router for navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import  StreamCreate from './streams/StreamCreate'
-import  StreamEdit from './streams/StreamEdit'
-import  StreamDelete from './streams/StreamDelete'
-import  StreamList from './streams/StreamList'
-import  StreamShow from './streams/StreamShow'
-import Header from './Header'
+import React from "react";
+import { Router, Route } from "react-router-dom";
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
+import history from "../history";
+
 const App = () => {
-  return(<div>
-    <div className='ui container'>
-    <BrowserRouter>
-    <Header/>
+  return (
     <div>
-      <Route path='/' exact component={StreamList} />
-      <Route path='/streams/new' exact component={StreamCreate} />
-      <Route path='/streams/edit' exact component={StreamEdit} />
-      <Route path='/streams/delete' exact component={StreamDelete} />
-      <Route path='/streams/show' exact component={StreamShow} />
+      <div className="ui container">
+        <Router history={history}>
+          <Header />
+          <div>
+            <Route path="/" exact component={StreamList} />
+            <Route path="/streams/new" exact component={StreamCreate} />
+            <Route path="/streams/edit" exact component={StreamEdit} />
+            <Route path="/streams/delete" exact component={StreamDelete} />
+            <Route path="/streams/show" exact component={StreamShow} />
+          </div>
+        </Router>
+      </div>
     </div>
-    </BrowserRouter>
-    </div>
-  </div>)
-}
-export default App
+  );
+};
+export default App;
