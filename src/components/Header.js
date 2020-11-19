71d3be3699ea061219e70fbcad73aa04ec54cd09
jhell85 <jhell85@gmail.com>
2020-11-19 07:51:23 -0800

    changed header title

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import GoogleAuth from './GoogleAuth'
+import React from "react";
+import { Link } from "react-router-dom";
+import GoogleAuth from "./GoogleAuth";
 
 const Header = () => {
   return (
-  <div className=" ui secondary pointing menu">
-    <Link to="/" className="item">
-      Streamer
-    </Link>
-    <div className='right menu'>
-    <Link to='/' className="item">
-      All Streams
-    </Link>
-    <GoogleAuth/>
+    <div className=" ui secondary pointing menu">
+      <Link to="/" className="item">
+        Streamers
+      </Link>
+      <div className="right menu">
+        <Link to="/" className="item">
+          All Streams
+        </Link>
+        <GoogleAuth />
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
 export default Header;
